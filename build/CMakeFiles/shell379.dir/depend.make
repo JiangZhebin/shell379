8ffec9555a632b0df6d6bb0e299937e8b71ad06c
JIANG Zhebin <jiangzhebin@JIANGs-Mac-mini.local>
2022-09-25 12:05:32 +0800

    remove build

--- build/CMakeFiles/shell379.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for shell379.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/shell_data_manager.dir/shell_data_manager.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/shell_data_manager.dir/shell_data_manager.cpp.o: \
-  /Users/jiangzhebin/cpp_bash/shell_data_manager.cpp
